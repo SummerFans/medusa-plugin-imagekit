b80a46492a6a442bd37c565df593fd166c5db6b1
summer <x.xiaokang@gmail.com>
2025-07-04 12:50:42 -0700

    fix(): fetch error

--- src/admin/routes/imagekit/page.tsx
+++ src/admin/routes/imagekit/page.tsx
@@ -9,9 +9,12 @@ declare const __BACKEND_URL__: string;
 
 export async function loader(): Promise<{ useStatistics: UsageStatisticsData }> {
 
-  const url = `${__BACKEND_URL__||''}/admin/plugin/imagekit/usage`
+  const url = `${__BACKEND_URL__ || ''}/admin/plugin/imagekit/usage`
 
-  const res = await fetch(url)
+  const res = await fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+  })
   const { result } = await res.json();
 
 
