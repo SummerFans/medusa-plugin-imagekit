5dac5147b72fb2e470c077d1ee0c18d4bca0baa4
summer <x.xiaokang@gmail.com>
2026-03-16 02:55:38 -0700

    change package.json

--- src/admin/components/usage-statistics/index.tsx
+++ src/admin/components/usage-statistics/index.tsx
@@ -1,8 +1,6 @@
 import { UsageStatisticsData } from '../../../providers/file-imagekit/types'
 import { formatUsageStatistics } from '../../utils'
 
-
-
 export default function UsageStatistics({ data = {
   bandwidthBytes: 0,
   mediaLibraryStorageBytes: 0,
@@ -16,8 +14,8 @@ export default function UsageStatistics({ data = {
     <div className="w-full p-4">
       <h3 className="text-base font-semibold text-gray-900 dark:text-white">Usage this month</h3>
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        {usageArr.map((item) => (
-          <div key={item.name} className="overflow-hidden rounded-lg bg-white dark:bg-zinc-800/50 px-4 py-5 shadow-sm sm:p-6">
+        {usageArr.map((item,index) => (
+          <div key={index} className="overflow-hidden rounded-lg bg-white dark:bg-zinc-800/50 px-4 py-5 shadow-sm sm:p-6">
             <dt title={item.name} className="truncate text-sm font-medium text-gray-500 dark:text-white/50">{item.name}</dt>
             <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900 dark:text-white/80">
               {item.value}
--- src/providers/file-imagekit/service.ts
+++ src/providers/file-imagekit/service.ts
@@ -1,13 +1,17 @@
 import { FileTypes } from "@medusajs/framework/types";
-import { AbstractFileProviderService, isDefined, MedusaError } from "@medusajs/framework/utils";
+import { Readable } from "node:stream";
+import {
+  AbstractFileProviderService,
+  isDefined,
+  MedusaError,
+} from "@medusajs/framework/utils";
 import { Logger } from "@medusajs/medusa";
-import ImageKit from "imagekit";
+import ImageKit from "@imagekit/nodejs";
 import { ImagekitOptions } from "./types";
 
 type InjectedDependencies = {
   logger: Logger;
 };
-
 
 export class ImagekitFileService extends AbstractFileProviderService {
   static identifier = "file-imagekit";
@@ -18,92 +22,97 @@ export class ImagekitFileService extends AbstractFileProviderService {
   static validateOptions(options: ImagekitOptions): void {
     if (!isDefined(options.publicKey)) {
       throw new MedusaError(
-        MedusaError.Types.INVALID_DATA, "Required option `publicKey` is missing in Imagekit plugin")
+        MedusaError.Types.INVALID_DATA,
+        "Required option `publicKey` is missing in Imagekit plugin",
+      );
     }
     if (!isDefined(options.privateKey)) {
       throw new MedusaError(
-        MedusaError.Types.INVALID_DATA, "Required option `privateKey` is missing in Imagekit plugin")
+        MedusaError.Types.INVALID_DATA,
+        "Required option `privateKey` is missing in Imagekit plugin",
+      );
     }
     if (!isDefined(options.imagekitID)) {
       throw new MedusaError(
-        MedusaError.Types.INVALID_DATA, "Required option `imagekitID` is missing in Imagekit plugin")
+        MedusaError.Types.INVALID_DATA,
+        "Required option `imagekitID` is missing in Imagekit plugin",
+      );
     }
   }
 
   constructor(
     { logger }: InjectedDependencies,
-    { publicKey, privateKey, imagekitID, folder, thumbnail }: ImagekitOptions
+    { publicKey, privateKey, imagekitID, folder, thumbnail }: ImagekitOptions,
   ) {
     super();
 
     this._logger = logger;
     this._option = { publicKey, privateKey, imagekitID, folder, thumbnail };
     if (thumbnail == undefined) {
-      this._option.thumbnail = true
+      this._option.thumbnail = true;
     }
 
     this._imagekit = new ImageKit({
-      publicKey: this._option.publicKey,
+      // publicKey: this._option.publicKey,
       privateKey: this._option.privateKey,
-      urlEndpoint: `https://ik.imagekit.io/${imagekitID}/`
+      // urlEndpoint: `https://ik.imagekit.io/${imagekitID}/`
     });
   }
 
-
   async upload(
-    file: FileTypes.ProviderUploadFileDTO
+    file: FileTypes.ProviderUploadFileDTO,
   ): Promise<FileTypes.ProviderFileResultDTO> {
-
     if (!file) {
-      throw new MedusaError(MedusaError.Types.INVALID_DATA, `No file provided`)
+      throw new MedusaError(MedusaError.Types.INVALID_DATA, `No file provided`);
     }
     try {
-      const content = Buffer.from(file.content, 'binary');
 
-      // const extension = file.filename.split('.').pop();
-      // const fileName = `${uuidv4()}.${extension}`;
+      const folder = (this._option.folder || "/medusa").replace(/\/$/gi, "");
 
-      const folder = (this._option.folder || '/medusa').replace(/\/$/ig, '');
-
-      const { fileId, url } = await this._imagekit.upload({
+      const { fileId, url } = await this._imagekit.files.upload({
         fileName: file.filename,
-        file: content,
+        file: file.content,
         folder,
-      })
+      });
 
-      this._logger.debug('complate upload \r\n')
+      this._logger.debug("completed upload \r\n");
 
-      let newURL = url;
+      let newURL = url || "";
       if (this._option.thumbnail) {
-        newURL = url.replace(folder, `${folder}/tr:w-300`)
+        newURL = url?.replace(folder, `${folder}/tr:w-300`) || "";
       }
 
-      return { url: newURL, key: fileId };
+      return { url: newURL, key: fileId || "" };
     } catch (e) {
-      throw new MedusaError("imagekit.upload.error", e?.message)
+      throw new MedusaError("imagekit.upload.error", e?.message);
     }
   }
 
-  async delete(files: FileTypes.ProviderDeleteFileDTO | FileTypes.ProviderDeleteFileDTO[]): Promise<void> {
-
+  async delete(
+    files: FileTypes.ProviderDeleteFileDTO | FileTypes.ProviderDeleteFileDTO[],
+  ): Promise<void> {
     if (!Array.isArray(files)) {
-      await this._imagekit.deleteFile(files.fileKey);
-      this._logger.debug("complate delete imagekit fileKey:" + files.fileKey);
+      await this._imagekit.delete(files.fileKey);
+      this._logger.debug("completed delete imagekit fileKey:" + files.fileKey);
     } else {
-      throw new MedusaError("imagekit.upload.error", "batch deletion is not supported")
+      throw new MedusaError(
+        "imagekit.upload.error",
+        "batch deletion is not supported",
+      );
     }
   }
 
-  async getPresignedDownloadUrl({ fileKey }: FileTypes.ProviderGetFileDTO): Promise<string> {
+  async getPresignedDownloadUrl({
+    fileKey,
+  }: FileTypes.ProviderGetFileDTO): Promise<string> {
+    // const filePath = await this._imagekit.get(fileKey);
 
-    const { filePath } = await this._imagekit.getFileDetails(fileKey);
+    // const signedUrl = await this._imagekit.url({
+    //   path: filePath,
+    //   signed: true,
+    //   expireSeconds: 1296000,
+    // });
 
-    const signedUrl = await this._imagekit.url({
-      path: filePath,
-      signed: true,
-      expireSeconds: 1296000
-    })
-
-    return signedUrl
+    return fileKey;
   }
 }
